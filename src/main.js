2912ed3133d0958969cbb4ce6e1e31327ff16bb9
Plínio Naves <pliniopjn@hotmail.com>
2018-09-29 15:49:56 -0300

    added @modus/ionic-vue integration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Ionic, IonicAPI } from '@modus/ionic-vue'
 import router from './router'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
+Ionic.init();
+Vue.use(IonicAPI)
+
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('ion-app')
